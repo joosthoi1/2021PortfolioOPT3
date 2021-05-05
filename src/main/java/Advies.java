930cad02c896de5d328606713b5f99b9ce4baa39
Joost Laoh <joost.laoh@outlook.com>
2021-05-05 21:33:19 +0200

    edited Suggestie.java

--- src/main/java/Advies.java
+++ src/main/java/Advies.java
@@ -9,5 +9,15 @@ public class Advies {
         return true;
     }
 
-
+    /**
+     * Returns a new Suggestie object of a certain category
+     *
+     * @param teamLeeftijd 0 = A leeftijd, 1 = B leeftijd
+     * @param klasse       0 = subtop, 1 = 1e klasse, 2 = 2e klasse
+     * @param type         the type of suggestion
+     * @param geslacht     0 = male, 1 = female
+     */
+    public Suggestie geefSuggestie (int teamLeeftijd, int klasse, String type, int geslacht) {
+        return new Suggestie(0);
+    }
 }
--- src/main/java/Suggestie.java
+++ src/main/java/Suggestie.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Suggestie {
+public class Suggestie {
+    private final int categorie;
+
+    public Suggestie(int categorie) {
+        this.categorie = categorie;
+    }
 }
