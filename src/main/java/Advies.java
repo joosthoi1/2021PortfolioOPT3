8bb8722f47b9674500699ef27e58612fc1406953
Joost Laoh <joost.laoh@outlook.com>
2021-05-05 20:54:59 +0200

    added Advies.java

---
+++ src/main/java/Advies.java
@@ -0,0 +1,11 @@
+public class Advies {
+    private String[] geveriefeerderClubs;
+
+    public Advies(String[] geveriefeerderClubs) {
+        this.geveriefeerderClubs = geveriefeerderClubs;
+    }
+
+    public boolean magAdviesKrijgen (boolean clubAccount, boolean adminAccount) {
+        return true;
+    }
+}
