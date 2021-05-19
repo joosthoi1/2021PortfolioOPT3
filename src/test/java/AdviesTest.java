945d3d72d56d97a2b4c853c176d8bfd8f915bab7
Joost Laoh <44155686+joosthoi1@users.noreply.github.com>
2021-05-19 21:20:39 +0200

    Revert "changed advies to a singleton class"

--- src/test/java/AdviesTest.java
+++ src/test/java/AdviesTest.java
@@ -1,14 +1,11 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AdviesTest {
     @Test
     void magAdviesKrijgenTest() {
-        Advies advies = Advies.getInstance();
-        advies.addClub("GMHC");
+        Advies advies = new Advies(new String[] {"GMHC"});
         assertTrue(advies.magAdviesKrijgen(true, "GMHC", false));
         assertFalse(advies.magAdviesKrijgen(false, "GMHC", false));
         assertFalse(advies.magAdviesKrijgen(true, "HCRB", false));
@@ -16,9 +13,7 @@ public class AdviesTest {
     }
     @Test
     void krijgAdviesTest() {
-        Advies advies = Advies.getInstance();
-        advies.addClub("GMHC");
-
+        Advies advies = new Advies(new String[] {"GMHC"});
         assertEquals(advies.geefSuggestie(0,0,"Pasing",0).getCategorie(), 1);
         assertEquals(advies.geefSuggestie(0,1,"Afronding",1).getCategorie(), 3);
         assertEquals(advies.geefSuggestie(1,0,"Afronding",0).getCategorie(), 2);
